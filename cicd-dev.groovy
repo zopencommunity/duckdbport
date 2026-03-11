02b2f1aa870f8d5d6f3a67f52285ecf674440010
v1gnesh <v1gnesh@users.noreply.github.com>
2026-03-11 08:31:55 +0530

    add cicd-dev.groovy
    Signed-off-by: v1gnesh <v1gnesh@users.noreply.github.com>

---
+++ cicd-dev.groovy
@@ -0,0 +1,6 @@
+node('linux')
+{
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/duckdbport.git'), string(name: 'PORT_DESCRIPTION', value: 'DuckDB is an in-process SQL OLAP Database Management System' ), string(name: 'BUILD_LINE', value: 'DEV'), , string(name: 'NODE_LABEL', value: "v3r1" )]
+  }
+}
